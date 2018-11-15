295acabce6a84cffe1554c332a9faa10fe3afbb7
sbonnefo persoMac <sbonnefo@student.42.fr>
2018-11-15 11:14:28 +0100

    add compatibility

--- app/src/main/java/com/sbonnefo/ft_hangouts/MainActivity.java
+++ app/src/main/java/com/sbonnefo/ft_hangouts/MainActivity.java
@@ -1,9 +1,12 @@
 package com.sbonnefo.ft_hangouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -15,21 +18,36 @@ public class MainActivity extends AppCompatActivity {
 
    // private LinearLayout _lytContactsView;
     private TextView _contactPreView, _phonePreView;
+    private Button btnGoContact;
     private DatabaseManager _databaseManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView( R.layout.activity_main );
 
         _contactPreView = (TextView) findViewById( R.id.txtContactPreView);
         _phonePreView = (TextView) findViewById( R.id.txtPhoneView);
+        btnGoContact = (Button) findViewById( R.id.btnContact);
+
+        btnGoContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, contactCard.class );
+                //Intent.putExtra("message", txtInputData.getText().toString() );
+                startActivity(intent);
+            }
+        });
+
+
         //_lytContactsVieux = ()
         _databaseManager = new DatabaseManager( this );
 //        Contact ctct = new Contact("BARELLE", "Margaux",
 //                "0613492146", "mago@gmail.com", "here");
 //        _databaseManager.insertContact(ctct);
+
+
         List<Contact> contacts = _databaseManager.getContacts();
         for (int i = 0; i <100 ; i++){
             for (Contact contact : contacts){
